0f1ef58189d4dc8e480f357a89da664d925c28fb
dinaemad88 <dinaemad287@gmail.com>
2023-09-27 20:13:16 +0200

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-<<<<<<< HEAD
+
 import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
 import { TourGuideRegisterComponent } from './auth/tour-guide-register/tour-guide-register.component';
@@ -26,22 +26,9 @@ import { TouristRegisterComponent } from './auth/Hotel-register/tourist-register
      TourGuideRegisterComponent,
      TouristRegisterComponent
      ],
-=======
-import { HotelsComponent } from './feature/hotels/hotels.component';
-import { CarouselComponent } from './shared/carousel/carousel.component';
-import { HotelDetailsComponent } from './feature/hotel-details/hotel-details.component';
-import { SearchComponent } from './shared/search/search.component';
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HotelsComponent,
-    CarouselComponent,
-    HotelDetailsComponent,
-    SearchComponent,
-  ],
->>>>>>> 53049a31340c6e8935bbe741e27a2d3f157a0436
+
+
+
   imports: [
     BrowserModule,
     AppRoutingModule,
