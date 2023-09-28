7dfd458401282e7b904881a92033c1327d6f42cc
Ahmed Shaaban Ahmed <108435080+ahmedshaaban1608@users.noreply.github.com>
2023-09-28 09:51:51 +0300

    fetch tour guide data from api

--- src/app/feature/services/tourguide-api.service.ts
+++ src/app/feature/services/tourguide-api.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TourguideApiService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  getTourGuideByUsername() {
+    return this.http.get(
+      'https://mocki.io/v1/4785a696-bed8-4985-a05d-80904de2987b'
+    );
+  }
 }
--- src/app/feature/tourguide-profile/tourguide-profile.component.ts
+++ src/app/feature/tourguide-profile/tourguide-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TourguideApiService } from '../services/tourguide-api.service';
 
 @Component({
   selector: 'app-tourguide-profile',
@@ -6,29 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./tourguide-profile.component.css'],
 })
 export class TourguideProfileComponent {
+  constructor(private tourguideApi: TourguideApiService) {}
   showMore: boolean = false;
   toggleDescriptionDisplay() {
     this.showMore = !this.showMore;
   }
-  tourguide: any = {
-    name: 'Ahmed Shaaban',
-    profile:
-      'https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg',
-    bio: 'Expert tour guide',
-    description: `<p>Hello Dear Traveler, My name is Wael , Im Egyptologist tours guide, an expert in all colors of tours around Egypt, supported with 15 years of experience (since 2005) , I do all tours not only in Cairo, to Giza, Alexandria, Saqqara, fayoum & Siwa oasis's Luxor and Aswan , I can arrange privet custmized tours for your family, friends, small group a memorable tailored privet Trip as the professional tour operators company will do , booking with great compatible fair prices , as a local Egyptian certificated Tour Guide capable on deliver any tourS to any city any where ,</p> <ul><li> Officially I Have ID certificates.</li><li>
-a member guide of the Egyptian General Tourist Guides Syndicate.</li>
-<li>a member guide of and the World Federation of Tour Guides Association and the Arab Tourist Guides Union.</li></ul></p>`,
-    areas: ['cairo', 'Giza', 'Hurghada', 'Sharm El-Shaikh'],
-    language: ['Arabic', 'English', 'German'],
-    background:
-      'https://www.presidency.eg/media/93222/big_12015_01_24_06_31_30jpg.jpg',
-    gallery: [
-      'https://images.jacobinmag.com/wp-content/uploads/sites/3/2023/03/02130051/50930840093_412705306d_k.jpg',
-      'https://cdn.mappr.co/wp-content/uploads/2023/06/cairo-aerial-view.jpg',
-      'https://cdn.britannica.com/47/194547-050-52813FB0/aerial-view-Cairo-Egypt.jpg',
-      'https://www.shandpartners.com/wp-content/uploads/2020/11/Egypt.jpg',
-      'https://img.traveltriangle.com/blog/wp-content/uploads/2018/12/Summer-In-Egypt-Cover.jpg',
-      'https://images.memphistours.com/large/a1941e50cb262271d6f737449e4d8233.jpg',
-    ],
-  };
+  tourguide: any = {};
+
+  ngOnInit() {
+    const data = this.tourguideApi
+      .getTourGuideByUsername()
+      .subscribe((data) => (this.tourguide = data));
+    console.log(data);
+  }
 }
