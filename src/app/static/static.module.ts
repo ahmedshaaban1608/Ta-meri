b016f0a33301758e92ece500aa81220f32c3dbac
Ahmed Shaaban Ahmed <108435080+ahmedshaaban1608@users.noreply.github.com>
2023-09-28 15:25:08 +0300

    add static pages content

--- src/app/static/static.module.ts
+++ src/app/static/static.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StaticRoutingModule } from './static-routing.module';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { TermsOfUseComponent } from './terms-of-use/terms-of-use.component';
+import { FAQsComponent } from './faqs/faqs.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PrivacyPolicyComponent,
+    TermsOfUseComponent,
+    FAQsComponent
+  ],
   imports: [
     CommonModule,
     StaticRoutingModule
