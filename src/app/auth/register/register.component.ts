9b7c3fbd0557e46230000aa8f41e1067c7fae25e
Ahmed Shaaban Ahmed <108435080+ahmedshaaban1608@users.noreply.github.com>
2023-09-28 19:56:29 +0300

    update

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -3,34 +3,36 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
   repeatPass: string = 'none';
 
   constructor() {}
 
-
-ngOnInit(): void{}
+  ngOnInit(): void {}
   RPWD: FormControl = new FormControl();
 
   registerForm = new FormGroup({
-    firstname: new FormControl("", [
+    firstname: new FormControl('', [
       Validators.required,
       Validators.minLength(4),
-      Validators.pattern(/^[a-zA-Z]{3,}(?:\s[a-zA-Z]{3,})*$/)
+      Validators.pattern(/^[a-zA-Z]{3,}(?:\s[a-zA-Z]{3,})*$/),
     ]),
     lastname: new FormControl('', [
       Validators.required,
       Validators.minLength(2),
-      Validators.pattern(/^[a-zA-Z]{3,}(?:\s[a-zA-Z]{3,})*$/)
+      Validators.pattern(/^[a-zA-Z]{3,}(?:\s[a-zA-Z]{3,})*$/),
     ]),
-    email: new FormControl('', [Validators.required,Validators.pattern( /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
+    ]),
     mobile: new FormControl('', [
       Validators.required,
-      Validators.pattern("[0-9]*"),
+      Validators.pattern('[0-9]*'),
       Validators.minLength(10),
-      Validators.maxLength(10)
+      Validators.maxLength(10),
     ]),
     gender: new FormControl('', [Validators.required]),
     pwd: new FormControl('', [
@@ -45,11 +47,11 @@ ngOnInit(): void{}
     if (this.registerForm.valid) {
       if (this.registerForm.value.pwd === this.registerForm.value.rpwd) {
         this.repeatPass = 'none';
-  
+
         const formData = JSON.stringify(this.registerForm.value);
         localStorage.setItem('formData', formData);
         console.log('Form data saved successfully!');
-        
+
         setTimeout(() => {
           window.location.href = '/login';
         }, 1000);
@@ -64,57 +66,59 @@ ngOnInit(): void{}
   showAlert() {
     const inputs = document.querySelectorAll('input');
     let allInputsFilled = true;
-  
-    inputs.forEach(input => {
+
+    inputs.forEach((input) => {
       if (!input.value) {
         allInputsFilled = false;
       }
     });
-  
-    if (allInputsFilled && this.registerForm.valid && this.registerForm.value.pwd === this.registerForm.value.rpwd) {
+
+    if (
+      allInputsFilled &&
+      this.registerForm.valid &&
+      this.registerForm.value.pwd === this.registerForm.value.rpwd
+    ) {
       alert('Registration successful!');
     }
- 
   }
-  
+
   allFieldsFilled(): boolean {
     const inputs = document.querySelectorAll('input');
     let allInputsFilled = true;
-  
-    inputs.forEach(input => {
+
+    inputs.forEach((input) => {
       if (!input.value) {
         allInputsFilled = false;
       }
     });
-  
+
     return allInputsFilled;
   }
-  
+
   get FirstName(): FormControl {
-    return this.registerForm.get("firstname") as FormControl;
+    return this.registerForm.get('firstname') as FormControl;
   }
 
   get LastName(): FormControl {
-    return this.registerForm.get("lastname") as FormControl;
+    return this.registerForm.get('lastname') as FormControl;
   }
 
   get Email(): FormControl {
-    return this.registerForm.get("email") as FormControl;
+    return this.registerForm.get('email') as FormControl;
   }
 
   get Mobile(): FormControl {
-    return this.registerForm.get("mobile") as FormControl;
+    return this.registerForm.get('mobile') as FormControl;
   }
 
   get Gender(): FormControl {
-    return this.registerForm.get("gender") as FormControl;
+    return this.registerForm.get('gender') as FormControl;
   }
 
   get Country(): FormControl {
-    return this.registerForm.get("country") as FormControl;
+    return this.registerForm.get('country') as FormControl;
   }
   get PWD(): FormControl {
-    return this.registerForm.get("pwd") as FormControl;
+    return this.registerForm.get('pwd') as FormControl;
   }
-
 }
