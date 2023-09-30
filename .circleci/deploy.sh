ab986f9875e39f69d19223092acce94091da4559
Ahmed Shaaban Ahmed <108435080+ahmedshaaban1608@users.noreply.github.com>
2023-10-01 01:28:28 +0300

    test deployment

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -8,4 +8,4 @@ FTP_HOST="${FTP_HOST}"
 REMOTE_DIR="${REMOTE_DIR}"
 
 echo "Uploading files to cPanel via FTP..."
-lftp -c "set ssl:verify-certificate no; open -u $FTP_USERNAME,$FTP_PASSWORD $FTP_HOST; mirror -R dist/ta-meri/* $REMOTE_DIR"
+lftp -c "set ssl:verify-certificate no; open -u $FTP_USERNAME,$FTP_PASSWORD $FTP_HOST; mirror -R dist/ta-meri/ $REMOTE_DIR"
