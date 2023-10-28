58ae94e077ff614c469f39ba6aec53260e0e555b
Ahmed Shaaban Ahmed <108435080+ahmedshaaban1608@users.noreply.github.com>
2023-10-28 20:46:17 +0200

    update

--- src/app/feature/services/hotel-api.service.ts
+++ src/app/feature/services/hotel-api.service.ts
@@ -1,14 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment.development';
-
 
 @Injectable({
   providedIn: 'root',
 })
 export class HotelApiService {
   constructor(private http: HttpClient) {}
-  hotelUrl: string = `${environment.apiUrl}/tourists`;
+  hotelUrl: string = `https://retoolapi.dev/7BRp9X/hotels`;
   getAllHotels() {
     return this.http.get(this.hotelUrl);
   }
