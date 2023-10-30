c3f9344d937f71fe24a92fb93b0e934af88bb181
Ahmed Shaaban Ahmed <108435080+ahmedshaaban1608@users.noreply.github.com>
2023-10-31 01:21:25 +0200

    add api for tourguide list

--- src/app/feature/services/guides-api.service.ts
+++ src/app/feature/services/guides-api.service.ts
@@ -2,49 +2,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { TourGuide } from '../interface/tour-guide';
 import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GuidesApiService {
-  private productsUrl = `${environment.apiUrl}/tourguides`;
-
-  constructor(private http: HttpClient) {}
-
-  getProducts(): Observable<any> {
-    return this.http.get(this.productsUrl);
-  }
-
-  getProductById(id: number): Observable<TourGuide | null> {
-    const productUrl = `${this.productsUrl}/${id}`;
-    return this.http.get<TourGuide>(productUrl).pipe(
-      map((product: TourGuide) => {
-        console.log('Fetched product:', product);
-        return product;
-      })
-    );
-  }
-
-  getTourGuide(id: string): Observable<TourGuide> {
-    const tourGuideUrl = `${this.productsUrl}/${id}`;
-    return this.http.get<TourGuide>(tourGuideUrl);
-  }
-
-  updateTourGuide(id: string, data: any): Observable<any> {
-    const tourGuideUrl = `${this.productsUrl}/${id}`;
-    return this.http.put(tourGuideUrl, data);
-  }
-}
-@Injectable({
-  providedIn: 'root',
-})
 export class TourguideApiService {
   constructor(private http: HttpClient) {}
   url = `${environment.apiUrl}/tourguides`;
   
+  getAllTourguides(): Observable<any> {
+    return this.http.get(`${this.url}`);
+
+  }
   getTourGuideById(id: number): Observable<any> {
     return this.http.get(`${this.url}/${id}`);
 
@@ -53,14 +23,8 @@ export class TourguideApiService {
   getTourGuideBySearch(word: string): Observable<any> {
     return this.http.get(`${this.url}?name=${word}`);
   }
-  
-  // getTourGuideReviews(): Observable<any> {
-  //   const reviewUrl = `${environment.apiUrl}/tourguides`;
-  // return this.http.get(reviewUrl);
-  // }
-  
-
-  // getTourGuideDataFromLink(link: string): Observable<any> {
-  //   return this.http.get(link);
-  // }
+  updateTourGuide(id: string, data: any): Observable<any> {
+    const tourGuideUrl = `${this.url}/${id}`;
+    return this.http.put(tourGuideUrl, data);
+  }
 }
